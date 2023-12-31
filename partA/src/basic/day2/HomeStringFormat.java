73cc545dbb9529409e1672515dfdc4dcc7fa666a
taewan28 <twk0223@gmail.com>
2023-12-31 23:49:25 +0900

    day2 소스

--- partA/src/basic/day2/HomeStringFormat.java
+++ partA/src/basic/day2/HomeStringFormat.java
@@ -2,8 +2,13 @@ package basic.day2;
 
 public class HomeStringFormat {
     public static void main(String[] args) {
-        int hoge = 1;
-    String str = String.format("변수hoge의 내용은%d이다.", hoge);
-        System.out.println(str);
+        int kim = 10;
+        double wer = 6.156;
+            String str = String.format("변수kim의 내용은 %d이다.", kim);
+            String tae = String.format("변수wer의 내용은 %f이다", wer);
+            String wan = String.format("TRUE%b, FALSE는 %b이다.", true, false);
+            System.out.println(str);
+            System.out.println(tae);
+            System.out.println(wan);
     }
 }
