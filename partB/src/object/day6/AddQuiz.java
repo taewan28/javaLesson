92ab2ff786371138ad26c9d9e2f73a6fa09d0fb8
taewan <twk0223@gamil.com>
2024-01-05 20:51:33 +0900

    day6 소스

--- partB/src/object/day6/AddQuiz.java
+++ partB/src/object/day6/AddQuiz.java
@@ -1,10 +1,48 @@
 package object.day6;
 
+import java.util.Arrays;
+
 public class AddQuiz {
 
-    private int datal;
+    private int data1;
     private int data2;
     private int userAnswer;
+    
+    public int getdata1() {
+        return this.data1;
+    }
+
+    public int getdata2() {
+        return this.data2;
+    }
+
+    public int getUserAnswer() {
+        return this.userAnswer;
+    }
+
+    public int getisRught() {
+        return this.userAnswer;
+    }
+    
+    
+    public void setdata1(int data1) {
+        this.data1 = data1;
+    }
+
+    public void setdata2(int data2) {
+        this.data2 = data2;
+        }
 
     
+    public void setUserAnswer(int userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+
+    boolean
+
+    public String getData(){
+        return String.format("%15s\t %10d\t %-10s\t %-20s", data1, data2, userAnswer);
+
+}
+
 }
--- partB/src/object/day6/AddQuziMain.java
+++ partB/src/object/day6/AddQuziMain.java
@@ -1,15 +1,37 @@
 package object.day6;
 
+import java.util.Scanner;
+
 public class AddQuziMain {
     public static void main(String[] args) {
     
-    
-    
-    
-        int start=1; int end=45;
-        for(int i=0; i<10;i++){
-            int temp = (int)(Math.random()*(end-start+1))+start;
-        System.out.println("1~45 범위의 난수 : " + temp);
-        }
+        Scanner sc = new Scanner(System.in);
+        
+
     }
 }
+
+        /* AddQuiz[] my = new AddQuiz[10];
+        my[0].setdata1(1);
+        System.out.println(my[0].getdata1());
+
+        System.out.println("덧셈 10문제 퀴즈를 시작합니다.");
+ */
+
+        //int start=11; int end=99;
+           /*  for(int i=0; i<10;i++){
+                int temp = (int)(Math.random()*(end-start+1))+start;
+                my[0].setdata1(1);
+                my[0].getdata1();
+                System.out.println(my[0].getdata1()); */
+           
+            /* for(int j=0; j<10;j++){
+                int temp2 = (int)(Math.random()*(end-start+1))+start;
+                my[0].setdata2(temp2);
+                my[0].getdata2();
+                System.out.println(temp1+"+"+temp2);
+        } */
+
+             //System.out.println("덧셈 10문제 퀴즈를 시작합니다.");
+
+        //System.out.println("11~99 범위의 난수 : " + temp1 + " + " + temp2 );
